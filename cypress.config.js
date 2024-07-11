22a6ab9ef3a2f737fae74936d65f36505afa2dd0
José Jean Piere Rodriguez Mejia <jrodriguez100196@gmail.com>
2024-07-11 06:24:51 +0000

    add  button run all specs

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
-module.exports = {
+// cypress.config.js
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    experimentalRunAllSpecs: true, // Habilitar la característica experimental
   },
-};
+});
